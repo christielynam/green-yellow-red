67c85e9bf62e0ee64e79f428e71dd424bcb054f2
Christie Lynam <christielynam@gmail.com>
2018-11-01 15:16:57 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Light = styled.div`
+  width: 200px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: black
+`
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      redOn: false,
+      yellowOn: false,
+      greenOn: true
+    }
+  }
+
+  timeout = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  slow = () => {  
+    this.setState({
+      redOn: false,
+      yellowOn: true,
+      greenOn: false
+    })
+  }
+
+  stop = () => {
+    setTimeout(() => {
+      this.setState({
+        redOn: true,
+        yellowOn: false,
+        greenOn: false
+      })
+    }, 2000)
+  }
+
+  async componentDidMount() {
+    await this.timeout(2000)
+    await this.slow()
+    this.stop()
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    const { redOn, yellowOn, greenOn } = this.state
+     return (
+      <Light>
+        <div className={redOn ? 'light red' : 'light'}></div>
+        <div className={yellowOn ? 'light yellow' : 'light'}></div>
+        <div className={greenOn ? 'light green' : 'light'}></div>
+      </Light>
     );
   }
 }
